89af50b85e0f2cf4c612cf300cb19ff3e0459e6c
Timothy Wang <timothywang@microsoft.com>
2024-08-20 14:49:52 -0400

    Cleanup

--- src/functions/browserValidation.js
+++ src/functions/browserValidation.js
@@ -10,9 +10,9 @@ const {
 } = require('../utils/constants');
 const { initializeChromium } = require('../helpers/chromiumHelper');
 
-function generateFailure(status, message) {
+function generateFailure(message) {
   return {
-    status,
+    status: 200,
     headers: {
       'Content-Type': 'application/json',
     },
@@ -33,7 +33,7 @@ async function browserValidationFunc(request, context) {
   generateLogMessage('', `Http function processed request for url "${request.url}"`);
 
   if (!request.body || request.body === null) {
-    return generateFailure(400, 'Invalid parameters: Missing request body');
+    return generateFailure('Invalid parameters: Missing request body');
   }
 
   let browser, page, requestBody;
@@ -50,7 +50,7 @@ async function browserValidationFunc(request, context) {
     generateLogMessage('', `Http function body: ${JSON.stringify(requestBody)}`);
   } catch (error) {
     context.error(`Error reading request body: ${error}`);
-    return generateFailure(400, 'Invalid JSON format in request body');
+    return generateFailure('Invalid JSON format in request body');
   }
 
   // STEP 1: Initialize puppeteer headless chromium
@@ -60,7 +60,7 @@ async function browserValidationFunc(request, context) {
     generateLogMessage(requestBody.hostname, 'Pupeteer headless chromium successfully launched');
   } catch (error) {
     context.error(`Error occurred: ${error}`);
-    return generateFailure(500, 'Error initializing Pupeteer headless chromium');
+    return generateFailure('Error initializing Pupeteer headless chromium');
   }
 
   // STEP 2: Navigate to request hostname
@@ -70,13 +70,18 @@ async function browserValidationFunc(request, context) {
     await page.goto(encodedURI);
   } catch (error) {
     context.error(`Error occurred: ${error}`);
-    return generateFailure(500, 'Error occurred navigating to the defaultHostname');
+    return generateFailure('Error occurred navigating to the defaultHostname');
   }
 
   // STEP 3: Get Credentials
   try {
     generateLogMessage(requestBody.hostname, 'Fetching secrets from keyvault');
     const inputCredentials = requestBody.credentials;
+    const keyvaultName = process.env.FUNCTIONS_RUNNER_KEY_VAULT_NAME;
+
+    if (!keyvaultName) {
+      return generateFailure('FUNCIONS_RUNNER_KEY_VAULT_NAME app setting not found, cannot get credentials');
+    }
 
     credentials[BASIC_AUTH_PASSWORD] =
       inputCredentials?.[BASIC_AUTH_PASSWORD_SECRET_NAME] &&
@@ -91,33 +96,34 @@ async function browserValidationFunc(request, context) {
       (await getSecretFromKeyVault('auth-testing-kv', inputCredentials[OIDC_AUTH_PASSWORD_SECRET_NAME]));
   } catch (error) {
     context.error(`Error occurred: ${error}`);
-    return generateFailure(500, 'Error occurred getting secrets from the keyvault');
+    return generateFailure('Error occurred getting secrets from the keyvault');
   }
 
   // STEP 4: Authenticate
   try {
     generateLogMessage(requestBody.hostname, 'Inputting authentication credentials');
 
-    if (await page.$('input[type="email"]')) {
+    const suppliedEmailCredentials = !!requestBody.credentials?.[OIDC_AUTH_EMAIL_SECRET_NAME];
+    if (suppliedEmailCredentials) {
       generateLogMessage(requestBody.hostname, 'Email input field found, inputting email');
 
-      await page.type('input[type="password"', credentials[OIDC_AUTH_EMAIL]);
+      await page.type('input[type="password"]', credentials[OIDC_AUTH_EMAIL]);
+
       // For Entra ID, we input email first and hit enter to input the password
-      if ((await page.$('input[type="password"')) === null) {
+      if ((await page.$('input[type="password"]')) === null) {
         await page.keyboard.press('Enter');
       }
     }
 
-    await page.waitForSelector('input[type="password"');
+    await page.waitForSelector('input[type="password"]');
 
     generateLogMessage(requestBody.hostname, 'Password input field found, inputting password');
-    await page.type('input[type="password"', credentials[BASIC_AUTH_PASSWORD] || credentials[OIDC_AUTH_PASSWORD]);
+    await page.type('input[type="password"]', credentials[BASIC_AUTH_PASSWORD] || credentials[OIDC_AUTH_PASSWORD]);
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
   } catch (error) {
     context.error(`Error occurred: ${error}. Page: ${await page.content()}`);
     return generateFailure(
-      500,
       `Error occurred during authentication, authentication page may not be ready yet. Check logs for more details. ${error}`,
     );
   }
@@ -147,19 +153,19 @@ async function browserValidationFunc(request, context) {
     }
 
     if (isError) {
-      return generateFailure(400, `Content validations failed for paths: ${failedValidationsPaths.join(',')}`);
+      return generateFailure(`Content validations failed for paths: ${failedValidationsPaths.join(',')}`);
     }
-
-    return {
-      status: 200,
-      body: JSON.stringify({
-        isError,
-      }),
-    };
   } catch (error) {
     context.error(`Error occurred: ${error}`);
-    return generateFailure(500, 'Error occurred during expected content validations');
+    return generateFailure('Error occurred during expected content validations');
   }
+
+  return {
+    status: 200,
+    body: JSON.stringify({
+      isError,
+    }),
+  };
 }
 
 app.http('runBrowserValidation', {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 const BASIC_AUTH_PASSWORD = "basicAuthPassword";
 const BASIC_AUTH_PASSWORD_SECRET_NAME = "basicAuthPasswordSecretName";
 const OIDC_AUTH_EMAIL = "oidcAuthEmail";
-const OIDC_AUTH_EMAIL_SECRET_NAME = "oidcAuthEmailSecretName";
+const OIDC_AUTH_EMAIL_SECRET_NAME = "emailSecretname";
 const OIDC_AUTH_PASSWORD = "oidcAuthPassword";
-const OIDC_AUTH_PASSWORD_SECRET_NAME = "oidcAuthPasswordSecretName";
+const OIDC_AUTH_PASSWORD_SECRET_NAME = "passwordSecretName";
 
 module.exports = {
     BASIC_AUTH_PASSWORD,
